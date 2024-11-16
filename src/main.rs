67d9411d1812173a830a411484efabb024d29722
Aarjav <68058926+AarjavPatni@users.noreply.github.com>
2024-11-16 17:41:53 -0500

    Implement query processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rust_decimal::Decimal;
+use std::collections::HashSet;
 use std::io;
 
 use crate::server::get_fills_api;
@@ -24,7 +26,44 @@ impl Processor {
     }
 
     pub fn process_query(&mut self, query: String) {
-        // TODO
-        println!("{}", query);
+        let query_parts = query.split_whitespace().collect::<Vec<&str>>();
+        if query_parts.len() != 3 {
+            eprintln!("Invalid query: {}", query);
+            return;
+        }
+
+        let query_type = query_parts[0];
+        let start_time = query_parts[1].parse::<i64>().unwrap();
+        let end_time = query_parts[2].parse::<i64>().unwrap();
+
+        let fills = get_fills_api(start_time, end_time).unwrap();
+
+        // print the count of fills for trades with unique sequence numbers and the given query type
+        // if there are multiple fills with the same sequence number, only count the fills once
+
+        let unique_fills = fills
+            .iter()
+            .map(|fill| (fill.sequence_number, fill.direction))
+            .collect::<HashSet<_>>();
+
+        match query_type {
+            "S" => println!(
+                "{:?}",
+                unique_fills.iter().filter(|fill| fill.1 == -1).count()
+            ),
+            "B" => println!(
+                "{:?}",
+                unique_fills.iter().filter(|fill| fill.1 == 1).count()
+            ),
+            "C" => println!("{:?}", unique_fills.len()),
+            "V" => println!(
+                "{:?}",
+                fills
+                    .iter()
+                    .map(|fill| fill.quantity * fill.price)
+                    .sum::<Decimal>()
+            ),
+            _ => println!("Invalid query type"),
+        }
     }
 }
